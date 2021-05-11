8360a8a33a4185f12ce093f1c73cf6bc2d0ca067
FlutLab.io <>
2021-05-11 03:43:55 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -82,7 +82,7 @@ class Inicio extends StatelessWidget {
                 Column(
                   children: <Widget>[
                     Padding(
-                      padding: EdgeInsets.all(10),
+                      padding: EdgeInsets.all(20),
                       child: RaisedButton(
                         color: Colors.pink[100],
                         shape: new RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
@@ -105,6 +105,27 @@ class Inicio extends StatelessWidget {
                   ],
                 ), //Fin columna interna
               ],
+                  Column(
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.all(10),
+                      child: RaisedButton(
+                        color: Colors.white,
+                        shape: new RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+                        onPressed: () {
+                          Navigator.pushNamed(context, "/empresa");
+                        },
+                        child: SizedBox(
+                          width: 100,
+                          height: 100,
+                          child: Center(
+                            child: Text("EMPRESA", textAlign: TextAlign.center),
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
             ),
           ],
         ),
